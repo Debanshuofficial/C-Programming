dafe3059ea9cc0e48333f8393e8c37044ca3e450
Debanshuofficial <121629639+Debanshuofficial@users.noreply.github.com>
2024-07-28 22:39:38 +0530

    Add files via upload

---
+++ Class 3/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a = 14, b = 13;
+    int add, mod, sub, div, multi, average;
+
+    add = a + b;
+    sub = a - b;
+    div = a/b;
+    multi = a*b;
+    average = add/2;
+ 
+    // printf("addition of the given two number is: %d\n", add);
+
+
+    printf("Sum of a and b : %d\n", add);
+    printf("Substraction of a and b : %d\n", sub);
+    printf("Division: %d, Multiplication: %d, Average: %d ", div, multi, average);
+
+
+    /* This is the comment of C programming */
+    
+    
+
+    return 0;
+}
